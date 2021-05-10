9830dffd513ecd75620b17b4ffe938f9ce6279d5
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 15:04:33 +0530

    start class has modefied for cache variable and methodCall() method

--- src/main/java/com/info/jwt/api/GitPracticeDemoForPrepApplication.java
+++ src/main/java/com/info/jwt/api/GitPracticeDemoForPrepApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitPracticeDemoForPrepApplication {
 
+	private Static HashMap<String,Integer> cache = new HashMap<>();
 	public static void main(String[] args) {
+		System.out.println("i am available now");
 		SpringApplication.run(GitPracticeDemoForPrepApplication.class, args);
 	}
-
+	public Integer methodCall(Integer methodId) {
+	System.out.println("it is returning method call id");
+	return methodId;
+	}
 }
