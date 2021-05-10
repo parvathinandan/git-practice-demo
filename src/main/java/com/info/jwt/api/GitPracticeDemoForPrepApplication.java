38cc239fa750538fef4b57a061c3758c769ae75c
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 14:56:28 +0530

    spring start class edited

--- src/main/java/com/info/jwt/api/GitPracticeDemoForPrepApplication.java
+++ src/main/java/com/info/jwt/api/GitPracticeDemoForPrepApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitPracticeDemoForPrepApplication {
 
+	private Static Integer companyId = 23;
 	public static void main(String[] args) {
 		SpringApplication.run(GitPracticeDemoForPrepApplication.class, args);
+	}
+
+	public void gitHelp() {
+	   System.out.println("i am out to get help for you");
 	}
 
 }
